82c2259085cceb2f0267432065cd0c3622845d66
Romain Jeanniot <jeanniot.webdev@icloud.com>
2025-04-11 18:38:58 +0200

    initial files

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div className="container-fluid gx-0 text-uppercase">
+        <nav className="navbar navbar-expand-lg bg-gradient bg-secondary shadow px-5 fixed-top">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Romain Jeanniot</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" aria-current="page" to="/">Accueil</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/services">Services</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/mention">Mentions légales</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+    }
+}
