11789ec0cc4e13f45c21766cc76deaf07e909fad
Romain Jeanniot <jeanniot.webdev@icloud.com>
2025-04-14 06:31:14 +0200

    Ajout composant Maps + intégration à page Contact Protection API avec variable d'environnement .env Installation package dotenv -> npm install dotenv --save

---
+++ src/Maps.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
+
+//const API_KEY = "AIzaSyAWPVZgD10jvMkWrlIMyKdIOcX7FU54zOk";
+
+const MyMap = () => (
+  <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+    <Map
+      defaultZoom={3}
+      defaultCenter={{lat: 48.85864659952736, lng: 2.298883837096165}} 
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+  </APIProvider>
+);
+export default MyMap;
